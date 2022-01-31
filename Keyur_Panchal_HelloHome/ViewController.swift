a857b1046cad6b2860a1c32a67afc99a7b030f3f
MrKeyur-CSE <keyur.developer.cse@gmail.com>
2022-01-31 01:55:25 -0500

    HelloHome Final Commit

--- Keyur_Panchal_HelloHome/ViewController.swift
+++ Keyur_Panchal_HelloHome/ViewController.swift
@@ -8,12 +8,27 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    //IBOutlets
+    @IBOutlet weak var helloHomeLabel: UILabel!
+    @IBOutlet var containerView: UIView!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
+    //IBActions
+    @IBAction func takeAction(_ sender: Any) {
+        print("Touched Button.")
+        
+        //Action with label
+        helloHomeLabel.text = "Bye Home!!"
+        helloHomeLabel.textColor = UIColor.red
+        containerView.backgroundColor = UIColor.cyan
+    }
+    
 
 }
 
